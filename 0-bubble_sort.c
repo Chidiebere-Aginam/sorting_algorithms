72ad753b5aa600c2d03eed969330efd441a3a064
Chidiebere-Aginam <chidiebereaginam@yahoo.com>
2023-02-21 19:35:41 +0100

    Sorting algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,39 +1,33 @@
-// C program for implementation of Bubble sort 
-#include <stdio.h> 
 #include "sort.h"
-  
+/**
+ *bubble_sort - Function that sorts an array of ints
+ *using bubble sort algorithm
+ *
+ *@array: array of ints
+ *@size: size of array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, k;
 
-void swap(int* xp, int* yp) 
-{ 
+	if (!array || !size)
+		return;
 
-    int temp = *xp; 
-
-    *xp = *yp; 
-
-    *yp = temp; 
-} 
-
-  
-// A function to implement bubble sort 
-
-void bubble_sort(int array[], size_t size) 
-{ 
-
-    size_t i, j; 
-
-    for (i = 0; i < size - 1; i++) 
-
-  
-
-        // Last i elements are already in place 
-
-        for (j = 0; j < size - i - 1; j++) {
-
-            if (array[j] > array[j + 1]) 
-
-                swap(&array[j], &array[j + 1]);
-    print_array(array, size);
-} 
+	i = 0;
+	while (i < size)
+	{
+		for (k = 0; k < size - 1; k++)
+		{
+			if (array[k] > array[k + 1])
+			{
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
 }
-
-  
